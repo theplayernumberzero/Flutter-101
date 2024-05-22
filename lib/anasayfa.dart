abc38fe93e8f40b5c64ac14b955567e38d037617
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-05-22 14:11:50 +0300

    row, column

--- lib/anasayfa.dart
+++ lib/anasayfa.dart
@@ -17,7 +17,42 @@ class _AnasayfaState extends State<Anasayfa> {
         backgroundColor: anaRenk,
         centerTitle: true,
       ),
-      body: const Center(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Text("Lominos", style: TextStyle(fontSize: 36, color: anaRenk, fontWeight: FontWeight.bold),),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Image.asset("resimler/pizza_resim.png",width: 350,height: 350,),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(onPressed: (){},
+                    style: TextButton.styleFrom(backgroundColor: anaRenk),
+                    child: Text("Cheese", style: TextStyle(color: yaziRenk1),),
+                ),
+                TextButton(onPressed: (){},
+                  style: TextButton.styleFrom(backgroundColor: anaRenk),
+                  child: Text("Sausage", style: TextStyle(color: yaziRenk1),),
+                ),
+                TextButton(onPressed: (){},
+                  style: TextButton.styleFrom(backgroundColor: anaRenk),
+                  child: Text("Olive", style: TextStyle(color: yaziRenk1),),
+                ),
+                TextButton(onPressed: (){},
+                  style: TextButton.styleFrom(backgroundColor: anaRenk),
+                  child: Text("Pepper", style: TextStyle(color: yaziRenk1),),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 }
